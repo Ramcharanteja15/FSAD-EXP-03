8ca0d5f6e7beaeed583a13b009edd77ed111bc3c
Ramcharanteja15 <ramcharanteja114@gmail.com>
2026-03-26 15:07:10 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import jakarta.persistence.";
+
+@Entity
+
+@Table(name="product")
+Public class product {
+private String name;
+
+private String description;
+
+private double price;
+
+private int quantity:
